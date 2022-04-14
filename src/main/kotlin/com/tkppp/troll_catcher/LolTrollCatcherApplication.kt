a6e621a56b594767b868b92aecb38a892e7bb05f
root <taekg.park@gmail.com>
2022-04-14 22:33:25 +0900

    init project

---
+++ src/main/kotlin/com/tkppp/troll_catcher/LolTrollCatcherApplication.kt
@@ -0,0 +1,11 @@
+package com.tkppp.troll_catcher
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LolTrollCatcherApplication
+
+fun main(args: Array<String>) {
+    runApplication<LolTrollCatcherApplication>(*args)
+}
---
+++ src/test/kotlin/com/tkppp/troll_catcher/LolTrollCatcherApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tkppp.troll_catcher
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LolTrollCatcherApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
