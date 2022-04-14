3932d784bbe545ab4b323ce3b52ed6336306e5b7
root <taekg.park@gmail.com>
2022-04-15 00:21:55 +0900

    set redis configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,10 +24,12 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
     implementation("com.github.ulisesbocchio:jasypt-spring-boot-starter:3.0.4")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("org.postgresql:postgresql")
---
+++ src/main/kotlin/com/tkppp/troll_catcher/config/redis/RedisConfig.kt
@@ -0,0 +1,35 @@
+package com.tkppp.troll_catcher.config.redis
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
+@Configuration
+class RedisConfig {
+
+    @Value("\${spring.redis.host}")
+    lateinit var host: String
+    private val port = 6379
+
+    @Bean
+    fun redisConnectionFactory(): RedisConnectionFactory =
+        LettuceConnectionFactory(host, port)
+
+    @Bean
+    fun redisTemplate(): RedisTemplate<String, String> {
+        val redisTemplate = RedisTemplate<String, String>()
+        redisTemplate.setConnectionFactory(redisConnectionFactory())
+        
+        redisTemplate.keySerializer = StringRedisSerializer()
+        redisTemplate.valueSerializer = StringRedisSerializer()
+
+        redisTemplate.hashKeySerializer = StringRedisSerializer()
+        redisTemplate.hashValueSerializer = StringRedisSerializer()
+
+        return redisTemplate
+    }
+}
