7526d9b842194af2da126234183e513acf1f10c6
root <taekg.park@gmail.com>
2022-04-15 00:12:04 +0900

    no message

--- src/test/kotlin/com/tkppp/troll_catcher/config/EncryptTest.kt
+++ src/test/kotlin/com/tkppp/troll_catcher/config/EncryptTest.kt
@@ -5,7 +5,6 @@ import org.jasypt.encryption.pbe.PooledPBEStringEncryptor
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
