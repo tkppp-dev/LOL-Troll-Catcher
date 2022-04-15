53cffcd4e4f84dad168171caca2f80b517c06aa6
root <taekg.park@gmail.com>
2022-04-15 21:31:48 +0900

    Mockk 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,8 @@ dependencies {
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.mockk:mockk:1.12.3")
+
 }
 
 tasks.withType<KotlinCompile> {
